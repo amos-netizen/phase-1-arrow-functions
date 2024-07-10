a8f941e38f69c2dc56262a0d61061d2672aeb025
amos <amosmokua12@gmail.com>
2024-07-10 13:05:04 +0300

    arrow functions

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-// Previous functions
-function scuberGreetingForFeet(distance) {
-    if (distance <= 400) {
-      return 'This one is on me!';
-    } else if (distance > 400 && distance <= 2000) {
-      return 'That will be twenty bucks.';
-    } else if (distance > 2000 && distance <= 2500) {
-      return 'I will gladly take your thirty bucks.';
-    } else {
-      return 'No can do.';
-    }
-  }
-  
-  function ternaryCheckCity(city) {
-    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-  }
-  
-  function switchOnCharmFromTip(tip) {
-    switch (tip) {
-      case 'generous':
-        return 'Thank you so much.';
-      case 'not as generous':
-        return 'Thank you.';
-      default:
-        return 'Bye.';
-    }
-  }
+
   
   // New functions
+  // Define the divide function expression
   const divide = function() {
     return 2000 / 100;
   };
-  
-  const square = (num) => num * num;
-  
-  const add = (a, b) => a + b;
+  // Define the square arrow function
+  const square = (x) => {
+    return x * x;
+  };
+  // Define the add arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
   
